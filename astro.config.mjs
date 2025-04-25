b0d074cca60f49970e677c88c48e12a578d787f5
Xu He <hexu90@gmail.com>
2025-04-25 15:53:28 -0700

    Add speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import { SITE_METADATA } from "./src/consts.ts";
 import metaTags from "astro-meta-tags";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: SITE_METADATA.siteUrl,
-  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()]
+  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()],
 });
